23748f772d1c3ef1437bfb79cd0211f513fcc4ea
thiru1709 <katasanithrinatha.reddy@gmail.com>
2025-07-13 11:19:44 +0530

    Refresh scope for config client

--- src/main/java/com/example/RefreshConfig/ApplicationConfig.java
+++ src/main/java/com/example/RefreshConfig/ApplicationConfig.java
@@ -5,8 +5,10 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
+@RefreshScope
 @ConfigurationProperties("microservice-one")
 @Configuration
 @Getter
